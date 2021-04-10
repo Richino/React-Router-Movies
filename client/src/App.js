a4388d947b7ccc445e80b678573de8d6e95d9edb
Richino <archerrichino@gmail.com>
2021-04-10 01:17:06 -0400

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Axios from "axios";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-import SavedList from './Movies/SavedList';
+export default function App() {
+	const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+	const [movieList, setMovieList] = useState([]);
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+	useEffect(() => {
+		const getMovies = () => {
+			Axios.get("http://localhost:5000/api/movies")
+				.then((res) => {
+					setMovieList(res.data);
+				})
+				.catch((err) => {
+					console.error("Server Error", err);
+				});
+		};
+		getMovies();
+	}, []);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	const addToSavedList = (id) => {
+		// This is stretch. Prevent the same movie from being "saved" more than once
+	};
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
-
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList
+				list={
+					[
+						/* This is stretch */
+					]
+				}
+			/>
+			<Switch>
+				<Route exact path="/">
+					<MovieList movies={movieList} />
+				</Route>
+				<Route exact path="/movies/:id">
+					<Movie />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
